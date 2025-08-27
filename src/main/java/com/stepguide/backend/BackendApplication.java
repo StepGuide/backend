b3a8f1d2a76b190ea6ea76e5f47a7c5945c4270a
SooHyeon Lee <dalsu4594@naver.com>
2025-08-27 13:23:54 +0900

    feat: MyBatis 설정 추가

--- src/main/java/com/stepguide/backend/BackendApplication.java
+++ src/main/java/com/stepguide/backend/BackendApplication.java
@@ -1,9 +1,11 @@
 package com.stepguide.backend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.stepguide.backend.domain.**.mapper")
 public class BackendApplication {
 
 	public static void main(String[] args) {
